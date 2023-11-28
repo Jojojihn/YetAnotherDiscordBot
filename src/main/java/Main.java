b920ee16d16b6556c7338a526926a0bd93d614bd
Jojojihn <jojojihn31@gmail.com>
2023-11-28 17:46:01 +0100

    Base dbmanager
    Setup base dbmanager functions and then created the server database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,27 @@
+import database.DBManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
     static JDA bot;
+    static DBManager dbManager;
     public static void main(String[] args) throws Exception{
         URL tokenUrl = Main.class.getResource("/token");
         assert tokenUrl != null;
         String token = Files.readString(Paths.get(tokenUrl.toURI()));
         bot = JDABuilder.createDefault(token, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.SCHEDULED_EVENTS )
                 .build();
+        bot.awaitReady();
         bot.getPresence().setActivity(Activity.competing("Amogus"));
+        DBManager dbManager = new DBManager();
+        dbManager.setup(bot);
     }
 }
