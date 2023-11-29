15bdd051342dc9c9010701f1299b2ae86e2bc8a3
Jojojihn <jojojihn31@gmail.com>
2023-11-29 12:35:02 +0100

    Create Abstract class for commands

---
+++ src/main/java/commands/Command.java
@@ -0,0 +1,9 @@
+package commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public abstract class Command {
+    public String name;
+    public String description;
+    public abstract void commandRun(SlashCommandInteractionEvent event);
+}
