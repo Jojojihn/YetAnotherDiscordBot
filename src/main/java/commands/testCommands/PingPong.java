22f718fc30caa804408d19d837a4d9a95096596e
Jojojihn <jojojihn31@gmail.com>
2023-11-29 12:35:17 +0100

    Create PingPong Command

---
+++ src/main/java/commands/testCommands/PingPong.java
@@ -0,0 +1,14 @@
+package commands.testCommands;
+
+import commands.Command;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class PingPong extends Command{
+    public PingPong(){
+        name = "ping";
+        description = "Poong";
+    }
+    public void commandRun(SlashCommandInteractionEvent event){
+        event.reply("Pong").queue();
+    }
+}
